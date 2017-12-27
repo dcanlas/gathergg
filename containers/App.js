32cca0186328177d63d0ccba0010529eb179c871
Dominique Canlas <canlasdominique@gmail.com>
2017-12-27 13:37:59 -0800

    Added scss compiler, react router, material ui

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,17 @@
 import React, {Component} from 'react';
+import '../css/main.scss'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+import MyAwesomeReactComponent from '../components/MyAwesomeReactComponent';
 
 export default class App extends Component {
     render () {
-        return <p>This is my new react app</p>
+        return (
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+                <AppBar title="My AppBar" />
+            </MuiThemeProvider>
+        )
     }
 }
--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter, Route, IndexRoute, hashHistory  } from 'react-router-dom'
 // main app
 import App from './containers/App';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const app = document.getElementById('app')
+
+ReactDOM.render(
+    <BrowserRouter history={hashHistory}>
+        <Route path="/" component={App} />
+    </BrowserRouter>,
+     app);

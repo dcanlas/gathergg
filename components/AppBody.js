220515355cadcf97da46cbe46e33712adbf58df8
Dominique Canlas <canlasdominique@gmail.com>
2017-12-29 15:19:52 -0800

    more stuff

---
+++ components/AppBody.js
@@ -0,0 +1,12 @@
+import React, { Component} from 'react';
+
+export default class AppBody extends Component {
+
+    render() {
+        return (
+            <div className="body-container">
+                hello world!
+            </div>
+        );
+    }
+}
--- containers/App.js
+++ containers/App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import '../css/main.scss'
+import '../css/reset.scss'
 import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
 import BottomNav from '../components/BottomNav';
+import AppBody from '../components/AppBody';
 
 export default class App extends Component {
     render () {
         return (
             <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-                <AppBar title="My AppBar" />
-                <BottomNav />
+                <div className="main-container">
+                    <AppBar title="My AppBar" />
+                    <AppBody />
+                    <BottomNav />
+                </div>
             </MuiThemeProvider>
         )
     }
